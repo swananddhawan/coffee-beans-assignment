13607f3b3fa8d1e5923646a93da7741701455ef1
Swanand Dhawan <swananddhawan@gmail.com>
2024-02-25 17:28:37 +0530

    Use default headers instead of custom method

--- app/lib/api/iterable/base.rb
+++ app/lib/api/iterable/base.rb
@@ -7,23 +7,19 @@ module Api
 
       raise_on [400, 401, 429, 500, 503]
 
+      # TODO: move key to env/secret
+      headers 'Content-Type' => 'application/json', 'X-Api-Key' => ITERABLE_API_KEY
+
       private
 
       def post_call!(path, body)
         body_with_camel_case_keys = deep_camelcase_keys(body)
-        response = self.class.post(path, headers: headers, body: body_with_camel_case_keys.to_json)
+        response = self.class.post(path, body: body_with_camel_case_keys.to_json)
         Response.new(response)
       end
 
       def deep_camelcase_keys(body)
         body.deep_transform_keys { |key| key.to_s.camelcase(:lower) }
-      end
-
-      def headers
-        {
-          'Content-Type' => 'application/json',
-          'X-Api-Key' => ITERABLE_API_KEY
-        }
       end
     end
   end
