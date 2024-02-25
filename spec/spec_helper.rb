f2bafb16deb78dc016a0b479fa8d1647bff9bdc6
Swanand Dhawan <swananddhawan@gmail.com>
2024-02-25 12:39:52 +0530

    Add `vcr` gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,3 +92,11 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
