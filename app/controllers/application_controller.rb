fae26ae1d7ad27930b9918d2e424b49d53743edb
Swanand Dhawan <swananddhawan@gmail.com>
2024-02-25 20:57:57 +0530

    Add exception handling of bad params for controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::ParameterMissing do |exception|
+    render status: :bad_request,
+           json: exception_response(exception)
+  end
+
+  private
+
+  def exception_response(exception)
+    {
+      data: nil,
+      message: exception.message,
+      errors: [exception]
+    }
+  end
+
   # NOTE: Currently, hard-coding this user-id for initial version.
   # This was created from the seed data.
   # Refer `db/seeds.rb` file for further details.
