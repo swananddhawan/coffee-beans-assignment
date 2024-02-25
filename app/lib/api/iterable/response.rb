db9904c2582b48973eb35b7709fe446ca4920819
Swanand Dhawan <swananddhawan@gmail.com>
2024-02-25 11:39:42 +0530

    Add Iterable response class

---
+++ app/lib/api/iterable/response.rb
@@ -0,0 +1,23 @@
+module Api
+  module Iterable
+    class Response
+      attr_reader :httparty_response,
+                  :parsed_response,
+                  :headers,
+                  :http_code,
+                  :iterable_code,
+                  :iterable_message,
+                  :iterable_params
+
+      def initialize(httparty_response)
+        @httparty_response = httparty_response
+        @parsed_response = JSON.parse(httparty_response, symbolize_names: true)
+        @headers = httparty_response.headers
+        @http_code = httparty_response.code
+        @iterable_code = parsed_response[:code]
+        @iterable_message = parsed_response[:msg]
+        @iterable_params = parsed_response[:params].to_h # For nil responses
+      end
+    end
+  end
+end
